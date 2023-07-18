21774f7a9c286106e4bd9c524ba48ef2dbf1f313
mkramers <mattkramers@gmail.com>
2023-07-17 23:33:02 -0400

    Add db tables

--- ftp-fun/db/migrations/20230718030315_init.sql
+++ ftp-fun/db/migrations/20230718030315_init.sql
@@ -1,5 +1,25 @@
 -- migrate:up
+CREATE TABLE connection (
+  id SERIAL PRIMARY KEY,
+  host TEXT,
+  user TEXT,
+  password TEXT
+);
 
+CREATE TABLE session (
+  id SERIAL PRIMARY KEY,
+  connection_id INT,
+  FOREIGN KEY(connection_id) REFERENCES connection(id)
+);
+
+CREATE TABLE directory_cache (
+  id SERIAL PRIMARY KEY,
+  session_id INT,
+  tree TEXT,
+  FOREIGN KEY(session_id) REFERENCES session(id)
+);
 
 -- migrate:down
-
+DELETE TABLE directory_cache;
+DELETE TABLE session;
+DELETE TABLE connection;
