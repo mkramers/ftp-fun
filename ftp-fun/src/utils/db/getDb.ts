49a38c1bd5aeb191ed59f76b8294161318e9dc97
mkramers <mattkramers@gmail.com>
2023-07-17 23:19:04 -0400

    Add db deps

---
+++ ftp-fun/src/utils/db/getDb.ts
@@ -0,0 +1,18 @@
+import { Database as SqliteDatabase, open } from "sqlite";
+import sqlite3 from "sqlite3";
+import * as fs from "fs";
+
+export type Database = SqliteDatabase<sqlite3.Database, sqlite3.Statement>;
+
+export async function getDb(filepath: string): Promise<Database> {
+  if (!fs.existsSync(filepath)) {
+    throw new Error(`Db does not exist at ${filepath}`);
+  }
+
+  console.log(`Opening database from ${filepath}...`);
+
+  return await open({
+    filename: filepath,
+    driver: sqlite3.Database,
+  });
+}
