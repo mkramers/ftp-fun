a383ef94a28659b40333c8f9aec1cbb7a58af5e7
mkramers <mattkramers@gmail.com>
2023-07-23 21:06:35 -0400

    Add disabled state and test button with logic

--- ftp-fun/src/app/components/Button/Button.tsx
+++ ftp-fun/src/app/components/Button/Button.tsx
@@ -3,17 +3,20 @@ import { PropsWithChildren } from "react";
 interface ButtonProps {
   onClick?: () => void;
   type?: "button" | "submit";
+  disabled?: boolean;
 }
 
 export function Button({
   onClick,
   type = "button",
+  disabled,
   children,
 }: PropsWithChildren<ButtonProps>) {
   return (
     <button
       type={type}
-      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+      disabled={disabled}
+      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:opacity-50"
       onClick={onClick}
     >
       {children}
--- ftp-fun/src/app/components/CreateConnection.tsx
+++ ftp-fun/src/app/components/CreateConnection.tsx
@@ -3,6 +3,7 @@ import { Connection } from "@/app/types/Connection";
 import { Button } from "@/app/components/Button/Button";
 import { Input } from "@/app/components/Input/Input";
 import { PartialBy } from "@/app/utils/types/PartialBy";
+import React, { useEffect, useState } from "react";
 
 type FormConnection = PartialBy<Connection, "verified">;
 
@@ -12,15 +13,28 @@ interface Props {
 }
 
 export function CreateConnection({ connection, onChanged }: Props) {
+  const [isVerified, setIsVerified] = useState(false);
+
+  useEffect(() => {
+    setIsVerified(connection?.verified ?? false);
+  }, [connection?.verified]);
+
   const {
     register,
     handleSubmit,
+    getValues,
     formState: { errors },
   } = useForm<FormConnection>({ defaultValues: connection });
 
   const onSubmit: SubmitHandler<FormConnection> = (connection) => {
     const verified = connection.verified ?? false;
     onChanged({ ...connection, verified });
+  };
+
+  const handleTestConnection = async () => {
+    const connection = getValues();
+
+    setIsVerified(true);
   };
 
   return (
@@ -65,7 +79,12 @@ export function CreateConnection({ connection, onChanged }: Props) {
         Password:{" "}
       </Input>
 
-      <Button type={"submit"}>Submit</Button>
+      <div className={"flex flex-row justify-center gap-2"}>
+        <Button onClick={handleTestConnection}>Test</Button>
+        <Button disabled={!isVerified} type={"submit"}>
+          Submit
+        </Button>
+      </div>
     </form>
   );
 }
