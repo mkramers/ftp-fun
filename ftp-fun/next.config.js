59a8728f8dad1041632a4d58ec7d7394e256f074
mkramers <mattkramers@gmail.com>
2023-07-26 22:30:20 -0400

    Fix sftp client

--- ftp-fun/next.config.js
+++ ftp-fun/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+  ) => {
+    if (!isServer) {
+      return config;
+    }
+
+    config.module.rules.push({
+      test: /\.node$/,
+      use: [
+        {
+          loader: "node-loader",
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 module.exports = nextConfig;
